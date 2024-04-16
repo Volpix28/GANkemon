0bff229cfa4115428a41c19500d3b076fc494fdb
Kibler Florian <di231503@fhstp.ac.at>
2024-04-16 19:10:13 +0200

    Add datatransformer

--- dataset/DataTransformer.py
+++ dataset/DataTransformer.py
@@ -1,9 +1,34 @@
+from PIL.Image import Resampling
 from datasets import load_dataset
+import os
+from tqdm import tqdm
+from PIL import Image
+
+
 class DataTransformer:
 
-    def __init__(self, root_path):
-        self.root_path = root_path
-        self.data = load_dataset("huggan/pokemon")
+    def __init__(self):
+        self.dataset = load_dataset("huggan/pokemon", split="train")
 
-    def transform_data(self):
-        pass
+    def transform_data(self, output_folder):
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        for i, example in enumerate(tqdm(self.dataset, desc="Processing images", unit="images")):
+            img = example["image"]
+
+            max_dim = max(img.width, img.height)
+
+            square_img = Image.new("RGB", (max_dim, max_dim), (255, 255, 255))
+
+            offset = ((max_dim - img.width) // 2, (max_dim - img.height) // 2)
+            square_img.paste(img, offset)
+
+            resized_img = square_img.resize((256, 256), Resampling.LANCZOS)
+
+            filename = os.path.join(output_folder, f"pokemon_{i}.png")
+            resized_img.save(filename)
+
+
+transformer = DataTransformer()
+transformer.transform_data("transformed_data")
